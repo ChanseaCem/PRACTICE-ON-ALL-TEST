1d657263c9ff751a922d61fe892874338252cd19
ChanseaCem <1030917584@qq.com>
2019-05-05 11:10:39 +0800

    1

--- test-pc/js/data.js
+++ test-pc/js/data.js
@@ -114,287 +114,391 @@
 }
 
 //数据统计
+//加载统计数据
 {
-	"Day": {
+	"ThreeMonth": {
+		"Efficiency": {
+			"One": {
+				"End_date": "2019-01-31",
+				"Oee": 0.547,
+				"Operate_Time_Rate": 0.52,
+				"Performance_Rate": 1.086,
+				"Pro_Num": 16745,
+				"Quality_Rate": 0.969,
+				"Start_date": "2019-01-01"
+			},
+			"Two": {
+				"End_date": "2019-02-28",
+				"Oee": 0,
+				"Operate_Time_Rate": 0.006,
+				"Performance_Rate": 0,
+				"Pro_Num": 0,
+				"Quality_Rate": 1,
+				"Start_date": "2019-02-01"
+			},
+			"Three": {
+				"End_date": "2019-03-31",
+				"Oee": 0.04,
+				"Operate_Time_Rate": 0.178,
+				"Performance_Rate": 0.227,
+				"Pro_Num": 1583,
+				"Quality_Rate": 1,
+				"Start_date": "2019-03-01"
+			}
+		},
+		"Status": {
+			"One": {
+				"Adjust_Time_Rate": 2167256,
+				"End_date": "2019-01-31",
+				"Fault_Time_Rate": 274703,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 4432613,
+				"Start_date": "2019-01-01",
+				"WarmUp_Time_Rate": 3853
+			},
+			"Two": {
+				"Adjust_Time_Rate": 337373,
+				"End_date": "2019-02-28",
+				"Fault_Time_Rate": 385239,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 6556,
+				"Start_date": "2019-02-01",
+				"WarmUp_Time_Rate": 0
+			},
+			"Three": {
+				"Adjust_Time_Rate": 7687030,
+				"End_date": "2019-03-31",
+				"Fault_Time_Rate": 543505,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 2178013,
+				"Start_date": "2019-03-01",
+				"WarmUp_Time_Rate": 246807
+			}
+		},
 		"Alarm": [{
-			"AlarmAlarm": 5333,
+			"AlarmCount": 490,
 			"AlarmMsg": "紧急停止状态"
 		}, {
-			"AlarmAlarm": 123,
-			"AlarmMsg": "启动"
+			"AlarmCount": 172,
+			"AlarmMsg": "红灯告警:"
+		}, {
+			"AlarmCount": 168,
+			"AlarmMsg": "急停状态"
+		}, {
+			"AlarmCount": 159,
+			"AlarmMsg": "红灯告警:G3011.3:机床加工中真空负压不足      "
+		}, {
+			"AlarmCount": 127,
+			"AlarmMsg": "紧急停止"
+		}, {
+			"AlarmCount": 113,
+			"AlarmMsg": "红灯告警:参数写入开关处于打开"
+		}, {
+			"AlarmCount": 90,
+			"AlarmMsg": "红灯告警:G3016.6 夹具二未松开到位"
+		}, {
+			"AlarmCount": 79,
+			"AlarmMsg": "红灯告警:G3016.12:主轴速度未到达，禁止G01移动"
+		}, {
+			"AlarmCount": 79,
+			"AlarmMsg": "红灯告警:(X)负向超程 (软超程1)"
+		}, {
+			"AlarmCount": 76,
+			"AlarmMsg": "红灯告警:G3011.10:真空报警！"
 		}],
+		"ErrorTime": [{
+			"Device_Id": "JT02",
+			"ErrTime": 111332
+		}, {
+			"Device_Id": "JT04",
+			"ErrTime": 307761
+		}, {
+			"Device_Id": "JT05",
+			"ErrTime": 18656
+		}, {
+			"Device_Id": "JT03",
+			"ErrTime": 58751
+		}, {
+			"Device_Id": "JT01",
+			"ErrTime": 46995
+		}]
+	},
+	"ThreeWeek": {
 		"Efficiency": {
 			"One": {
-				"End_date": "2019-04-121",
-				"Oee": 0.1161,
-				"Operate_Time_Rate": 0.1156,
-				"Performance_Rate": 1.1031,
-				"Pro_Num": 1215,
-				"Quality_Rate": 1,
-				"Start_date": "2019-04-121"
+				"End_date": "2019-04-14",
+				"Oee": 0.17,
+				"Operate_Time_Rate": 0.17,
+				"Performance_Rate": 1.011,
+				"Pro_Num": 1606,
+				"Quality_Rate": 0.991,
+				"Start_date": "2019-04-08"
 			},
 			"Two": {
-				"End_date": "2019-04-122",
-				"Oee": 0.2161,
-				"Operate_Time_Rate": 0.2156,
-				"Performance_Rate": 1.2031,
-				"Pro_Num": 2215,
-				"Quality_Rate": 21,
-				"Start_date": "2019-04-122"
+				"End_date": "2019-04-21",
+				"Oee": 0.054,
+				"Operate_Time_Rate": 0.185,
+				"Performance_Rate": 0.291,
+				"Pro_Num": 504,
+				"Quality_Rate": 0.997,
+				"Start_date": "2019-04-15"
 			},
 			"Three": {
-				"End_date": "2019-04-123",
-				"Oee": 0.3161,
-				"Operate_Time_Rate": 0.3156,
-				"Performance_Rate": 1.3031,
-				"Pro_Num": 3215,
-				"Quality_Rate": 31,
-				"Start_date": "2019-04-123"
-			},
-			"Four": {
-				"End_date": "2019-04-124",
-				"Oee": 0.4161,
-				"Operate_Time_Rate": 0.4156,
-				"Performance_Rate": 1.4031,
-				"Pro_Num": 4215,
-				"Quality_Rate": 41,
-				"Start_date": "2019-04-124"
-			},
-			"Five": {
-				"End_date": "2019-04-125",
-				"Oee": 0.5161,
-				"Operate_Time_Rate": 0.5156,
-				"Performance_Rate": 1.5031,
-				"Pro_Num": 5215,
-				"Quality_Rate": 51,
-				"Start_date": "2019-04-125"
-			},
-			"Six": {
-				"End_date": "2019-04-126",
-				"Oee": 0.6161,
-				"Operate_Time_Rate": 0.6156,
-				"Performance_Rate": 1.6031,
-				"Pro_Num": 6215,
-				"Quality_Rate": 61,
-				"Start_date": "2019-04-126"
-			},
-			"Seven": {
-				"End_date": "2019-04-127",
-				"Oee": 0.7161,
-				"Operate_Time_Rate": 0.7156,
-				"Performance_Rate": 1.7031,
-				"Pro_Num": 7215,
-				"Quality_Rate": 71,
-				"Start_date": "2019-04-127"
+				"End_date": "2019-04-28",
+				"Oee": 0.028,
+				"Operate_Time_Rate": 0.033,
+				"Performance_Rate": 0.857,
+				"Pro_Num": 263,
+				"Quality_Rate": 0.997,
+				"Start_date": "2019-04-22"
 			}
 		},
+		"Status": {
+			"One": {
+				"Adjust_Time_Rate": 921901,
+				"End_date": "2019-04-14",
+				"Fault_Time_Rate": 38901,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 512957,
+				"Start_date": "2019-04-08",
+				"WarmUp_Time_Rate": 0
+			},
+			"Two": {
+				"Adjust_Time_Rate": 1882391,
+				"End_date": "2019-04-21",
+				"Fault_Time_Rate": 114400,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 558739,
+				"Start_date": "2019-04-15",
+				"WarmUp_Time_Rate": 0
+			},
+			"Three": {
+				"Adjust_Time_Rate": 2053362,
+				"End_date": "2019-04-28",
+				"Fault_Time_Rate": 400380,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 99134,
+				"Start_date": "2019-04-22",
+				"WarmUp_Time_Rate": 0
+			}
+		},
+		"Alarm": [{
+			"AlarmCount": 132,
+			"AlarmMsg": "紧急停止状态"
+		}, {
+			"AlarmCount": 85,
+			"AlarmMsg": "红灯告警:G  代码不正确"
+		}, {
+			"AlarmCount": 65,
+			"AlarmMsg": "红灯告警:G3011.3:机床加工中真空负压不足      "
+		}, {
+			"AlarmCount": 50,
+			"AlarmMsg": "红灯告警:目标点在负限位外"
+		}, {
+			"AlarmCount": 49,
+			"AlarmMsg": "红灯告警:目标点在正限位外"
+		}, {
+			"AlarmCount": 42,
+			"AlarmMsg": "气压异常不能转动主轴"
+		}, {
+			"AlarmCount": 30,
+			"AlarmMsg": "红灯告警:有刀具寿命已完"
+		}, {
+			"AlarmCount": 29,
+			"AlarmMsg": "红灯告警:G3014.3:油冷机报警(X0.4)                 "
+		}, {
+			"AlarmCount": 27,
+			"AlarmMsg": "红灯告警:G3016.4 夹具二未夹紧到位"
+		}, {
+			"AlarmCount": 24,
+			"AlarmMsg": "红灯告警:(X)负向超程 (软超程1)"
+		}],
 		"ErrorTime": [{
 			"Device_Id": "JT03",
-			"ErrTime": 13
-		}],
-		"Status": {
+			"ErrTime": 10224
+		}, {
+			"Device_Id": "JT01",
+			"ErrTime": 8717
+		}, {
+			"Device_Id": "JT02",
+			"ErrTime": 96198
+		}, {
+			"Device_Id": "JT04",
+			"ErrTime": 394
+		}, {
+			"Device_Id": "JT05",
+			"ErrTime": 787
+		}]
+	},
+	"Day": {
+		"Efficiency": {
 			"One": {
-				"Adjust_Time_Rate": 113316,
-				"End_date": "12019-04-12",
-				"Fault_Time_Rate": 116,
-				"Idle_Time_Rate": 10,
-				"Real_Process_Time_Rate": 167335,
-				"Start_date": "12019-04-12",
-				"WarmUp_Time_Rate": 10
+				"End_date": "2019-04-23",
+				"Oee": 0.087,
+				"Operate_Time_Rate": 0.096,
+				"Performance_Rate": 0.926,
+				"Pro_Num": 119,
+				"Quality_Rate": 0.98,
+				"Start_date": "2019-04-23"
 			},
 			"Two": {
-				"Adjust_Time_Rate": 213316,
-				"End_date": "22019-04-12",
-				"Fault_Time_Rate": 216,
-				"Idle_Time_Rate": 20,
-				"Real_Process_Time_Rate": 267335,
-				"Start_date": "22019-04-12",
-				"WarmUp_Time_Rate": 20
+				"End_date": "2019-04-24",
+				"Oee": 0.075,
+				"Operate_Time_Rate": 0.083,
+				"Performance_Rate": 0.902,
+				"Pro_Num": 100,
+				"Quality_Rate": 1,
+				"Start_date": "2019-04-24"
 			},
 			"Three": {
-				"Adjust_Time_Rate": 313316,
-				"End_date": "32019-04-12",
-				"Fault_Time_Rate": 316,
-				"Idle_Time_Rate": 30,
-				"Real_Process_Time_Rate": 367335,
-				"Start_date": "32019-04-12",
-				"WarmUp_Time_Rate": 30
+				"End_date": "2019-04-25",
+				"Oee": 0.022,
+				"Operate_Time_Rate": 0.026,
+				"Performance_Rate": 0.832,
+				"Pro_Num": 29,
+				"Quality_Rate": 1,
+				"Start_date": "2019-04-25"
 			},
 			"Four": {
-				"Adjust_Time_Rate": 413316,
-				"End_date": "42019-04-12",
-				"Fault_Time_Rate": 416,
-				"Idle_Time_Rate": 40,
-				"Real_Process_Time_Rate": 467335,
-				"Start_date": "42019-04-12",
-				"WarmUp_Time_Rate": 40
+				"End_date": "2019-04-26",
+				"Oee": 0.01,
+				"Operate_Time_Rate": 0.021,
+				"Performance_Rate": 0.495,
+				"Pro_Num": 14,
+				"Quality_Rate": 1,
+				"Start_date": "2019-04-26"
 			},
 			"Five": {
-				"Adjust_Time_Rate": 513316,
-				"End_date": "52019-04-12",
-				"Fault_Time_Rate": 516,
-				"Idle_Time_Rate": 50,
-				"Real_Process_Time_Rate": 567335,
-				"Start_date": "52019-04-12",
-				"WarmUp_Time_Rate": 50
+				"End_date": "2019-04-27",
+				"Oee": 0,
+				"Operate_Time_Rate": 0,
+				"Performance_Rate": 0,
+				"Pro_Num": 0,
+				"Quality_Rate": 1,
+				"Start_date": "2019-04-27"
 			},
 			"Six": {
-				"Adjust_Time_Rate": 613316,
-				"End_date": "6019-04-12",
-				"Fault_Time_Rate": 616,
-				"Idle_Time_Rate": 60,
-				"Real_Process_Time_Rate": 667335,
-				"Start_date": "62019-04-12",
-				"WarmUp_Time_Rate": 60
+				"End_date": "2019-04-28",
+				"Oee": 0,
+				"Operate_Time_Rate": 0,
+				"Performance_Rate": 0,
+				"Pro_Num": 0,
+				"Quality_Rate": 1,
+				"Start_date": "2019-04-28"
 			},
 			"Seven": {
-				"Adjust_Time_Rate": 713316,
-				"End_date": "7019-04-12",
-				"Fault_Time_Rate": 716,
-				"Idle_Time_Rate": 70,
-				"Real_Process_Time_Rate": 767335,
-				"Start_date": "72019-04-12",
-				"WarmUp_Time_Rate": 70
-			}
-		}
-	},
-	"ThreeMonth": {
-		"Alarm": [{
-			"AlarmCount": 5333,
-			"AlarmMsg": "紧急停止状态"
-		}],
-		"Efficiency": {
-			"One": {
-				"End_date": "2019-04-121",
-				"Oee": 0.1161,
-				"Operate_Time_Rate": 0.1156,
-				"Performance_Rate": 1.1031,
-				"Pro_Num": 1215,
+				"End_date": "2019-04-29",
+				"Oee": 0.016,
+				"Operate_Time_Rate": 0.041,
+				"Performance_Rate": 0.4,
+				"Pro_Num": 22,
 				"Quality_Rate": 1,
-				"Start_date": "2019-04-121"
-			},
-			"Two": {
-				"End_date": "2019-04-122",
-				"Oee": 0.2161,
-				"Operate_Time_Rate": 0.2156,
-				"Performance_Rate": 1.2031,
-				"Pro_Num": 2215,
-				"Quality_Rate": 21,
-				"Start_date": "2019-04-122"
-			},
-			"Three": {
-				"End_date": "2019-04-123",
-				"Oee": 0.3161,
-				"Operate_Time_Rate": 0.3156,
-				"Performance_Rate": 1.3031,
-				"Pro_Num": 3215,
-				"Quality_Rate": 31,
-				"Start_date": "2019-04-123"
+				"Start_date": "2019-04-29"
 			}
 		},
-		"ErrorTime": [{
-			"Device_Id": "JT03"
-			"ErrTime": 13
-		}],
 		"Status": {
 			"One": {
-				"Adjust_Time_Rate": 113316,
-				"End_date": "12019-04-12",
-				"Fault_Time_Rate": 116,
-				"Idle_Time_Rate": 10,
-				"Real_Process_Time_Rate": 167335,
-				"Start_date": "12019-04-12",
-				"WarmUp_Time_Rate": 10
+				"Adjust_Time_Rate": 204488,
+				"End_date": "2019-04-23",
+				"Fault_Time_Rate": 52207,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 41500,
+				"Start_date": "2019-04-23",
+				"WarmUp_Time_Rate": 0
 			},
 			"Two": {
-				"Adjust_Time_Rate": 213316,
-				"End_date": "22019-04-12",
-				"Fault_Time_Rate": 216,
-				"Idle_Time_Rate": 20,
-				"Real_Process_Time_Rate": 267335,
-				"Start_date": "22019-04-12",
-				"WarmUp_Time_Rate": 20
+				"Adjust_Time_Rate": 301596,
+				"End_date": "2019-04-24",
+				"Fault_Time_Rate": 7009,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 35808,
+				"Start_date": "2019-04-24",
+				"WarmUp_Time_Rate": 0
 			},
 			"Three": {
-				"Adjust_Time_Rate": 313316,
-				"End_date": "32019-04-12",
-				"Fault_Time_Rate": 316,
-				"Idle_Time_Rate": 30,
-				"Real_Process_Time_Rate": 367335,
-				"Start_date": "32019-04-12",
-				"WarmUp_Time_Rate": 30
-			}
-		}
-	}
-	"ThreeWeek": {
-		"Alarm": [{
-			"AlarmCount": 5333,
-			"AlarmMsg": "紧急停止状态"
-		}],
-		"Efficiency": {
-			"One": {
-				"End_date": "2019-04-121",
-				"Oee": 0.1161,
-				"Operate_Time_Rate": 0.1156,
-				"Performance_Rate": 1.1031,
-				"Pro_Num": 1215,
-				"Quality_Rate": 1,
-				"Start_date": "2019-04-121"
+				"Adjust_Time_Rate": 324646,
+				"End_date": "2019-04-25",
+				"Fault_Time_Rate": 557,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 11254,
+				"Start_date": "2019-04-25",
+				"WarmUp_Time_Rate": 0
 			},
-			"Two": {
-				"End_date": "2019-04-122",
-				"Oee": 0.2161,
-				"Operate_Time_Rate": 0.2156,
-				"Performance_Rate": 1.2031,
-				"Pro_Num": 2215,
-				"Quality_Rate": 21,
-				"Start_date": "2019-04-122"
+			"Four": {
+				"Adjust_Time_Rate": 380769,
+				"End_date": "2019-04-26",
+				"Fault_Time_Rate": 1079,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 9139,
+				"Start_date": "2019-04-26",
+				"WarmUp_Time_Rate": 0
 			},
-			"Three": {
-				End_date: "2019-04-123",
-				Oee: 0.3161,
-				Operate_Time_Rate: 0.3156,
-				Performance_Rate: 1.3031,
-				Pro_Num: 3215,
-				Quality_Rate: 31,
-				Start_date: "2019-04-123"
+			"Five": {
+				"Adjust_Time_Rate": 432000,
+				"End_date": "2019-04-27",
+				"Fault_Time_Rate": 0,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 0,
+				"Start_date": "2019-04-27",
+				"WarmUp_Time_Rate": 0
+			},
+			"Six": {
+				"Adjust_Time_Rate": 359599,
+				"End_date": "2019-04-28",
+				"Fault_Time_Rate": 72960,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 0,
+				"Start_date": "2019-04-28",
+				"WarmUp_Time_Rate": 0
+			},
+			"Seven": {
+				"Adjust_Time_Rate": 333678,
+				"End_date": "2019-04-29",
+				"Fault_Time_Rate": 8396,
+				"Idle_Time_Rate": 0,
+				"Real_Process_Time_Rate": 17751,
+				"Start_date": "2019-04-29",
+				"WarmUp_Time_Rate": 0
 			}
 		},
-		"ErrorTime": [{
-			Device_Id: "JT03"
-			ErrTime: 13
+		"Alarm": [{
+			"AlarmCount": 16,
+			"AlarmMsg": "红灯告警:G3011.3:机床加工中真空负压不足      "
+		}, {
+			"AlarmCount": 8,
+			"AlarmMsg": "红灯告警:G3011.11:气压报警(X0.7)     "
+		}, {
+			"AlarmCount": 6,
+			"AlarmMsg": "红灯告警:"
+		}, {
+			"AlarmCount": 5,
+			"AlarmMsg": "告警:1015: 气压报警!"
+		}, {
+			"AlarmCount": 2,
+			"AlarmMsg": "告警:4:程序的开头无地址"
+		}, {
+			"AlarmCount": 2,
+			"AlarmMsg": "G3011.3:机床加工中真空负压不足      "
+		}, {
+			"AlarmCount": 1,
+			"AlarmMsg": "G3011.11:气压报警(X0.7)     "
 		}],
-		"Status": {
-			"One": {
-				Adjust_Time_Rate: 113316,
-				End_date: "12019-04-12",
-				Fault_Time_Rate: 116,
-				Idle_Time_Rate: 10,
-				Real_Process_Time_Rate: 167335,
-				Start_date: "12019-04-12",
-				WarmUp_Time_Rate: 10
-			},
-			"Two": {
-				Adjust_Time_Rate: 213316,
-				End_date: "22019-04-12",
-				Fault_Time_Rate: 216,
-				Idle_Time_Rate: 20,
-				Real_Process_Time_Rate: 267335,
-				Start_date: "22019-04-12",
-				WarmUp_Time_Rate: 20
-			},
-			"Three": {
-				Adjust_Time_Rate: 313316,
-				End_date: "32019-04-12",
-				Fault_Time_Rate: 316,
-				Idle_Time_Rate: 30,
-				Real_Process_Time_Rate: 367335,
-				Start_date: "32019-04-12",
-				WarmUp_Time_Rate: 30
-			}
-		}
+		"ErrorTime": [{
+			"Device_Id": "JT04",
+			"ErrTime": 19
+		}, {
+			"Device_Id": "JT05",
+			"ErrTime": 144
+		}, {
+			"Device_Id": "JT03",
+			"ErrTime": 4168
+		}, {
+			"Device_Id": "JT01",
+			"ErrTime": 4065
+		}]
 	}
-} {
+}
+{
 	"Day": {
 		"Alarm": [],
 		"Efficiency": {
--- test-pc/js/test.js
+++ test-pc/js/test.js
@@ -1,1 +1,34 @@
-console.log("进程 " + process.argv[2] + " 执行。" );
+//更新月告警时间统计
+var errtMonthData = [];
+var merrt_data = data.ThreeMonth.ErrorTime;
+		//console.log(merrt_data);
+		if(merrt_data) {
+			merrt_data.sort(compare('ErrTime'));
+		}
+		for(var key in merrt_data) {
+			key = merrt_data.length - key - 1;
+			if(merrt_data[key] && typeof merrt_data[key] == 'object') {
+				errtMonthData.push({
+					"country": merrt_data[key].Device_Id,
+					"value": merrt_data[key].ErrTime
+					// "timeValue":intToTime(merrt_data[key].ErrTime)
+				})
+			}
+		}
+		_this.loadMonthErrTimeChart(errtMonthData);
+		//更新月告警次数统计
+		var errcMonthData = [];
+		var merrc_data = data.ThreeMonth.Alarm;
+		for(var key in merrc_data) {
+			if(merrc_data[key] && typeof merrc_data[key] == 'object') {
+				errcMonthData.push({
+					"country": merrc_data[key].AlarmMsg,
+					"value": merrc_data[key].AlarmCount
+				})
+			}
+		}
+
+		{
+			country: "JT04"
+			value: 307761
+		}
