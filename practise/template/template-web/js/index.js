20e1b09fbf5dade80673244bcafb9ee41c95c196
ChanseaCem <1030917584@qq.com>
2019-09-30 10:56:41 +0800

    添加js模板

---
+++ practise/template/template-web/js/index.js
@@ -0,0 +1,47 @@
+var _indexModule = function() {
+	var _this = this;
+
+	this.init = function() {
+		_this.UIView();
+		_this.onEvent();
+		_this.updateCon();
+	}
+
+	/**
+	 * 初始化界面排版和内容
+	 */
+	this.UIView = function() {
+
+	}
+
+	/**
+	 * 事件初始化监听
+	 */
+	this.onEvent = function() {
+
+	}
+
+	/**
+	 * 初始化内容数据
+	 */
+	this.updateCon = function() {
+
+	}
+
+	/**
+	 * 各种数据请求
+	 */
+	this.objData = {
+
+	}
+
+	/**
+	 * 内容数据操作
+	 */
+	this.operationCon = {
+
+	}
+}
+
+window._IndexModule = new _indexModule();
+window._IndexModule.init();
